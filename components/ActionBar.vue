c08c967261e5cbe2371711512ad13e777882d7f0
Errorist79 <klarendon@outlook.com>
2025-11-28 15:22:59 +0300

    refactor(action-bar): remove logout button logic and mainnet check

--- components/ActionBar.vue
+++ components/ActionBar.vue
@@ -9,20 +9,11 @@ import Search from "@/components/modules/navigation/Search.vue"
 
 /** Utils */
 import { isMobile } from "@/services/utils"
-import { isMainnet } from "@/services/utils/general"
 
 /** Store */
 import { useAppStore } from "@/store/app.store"
 const appStore = useAppStore()
 
-/** Auth */
-const { logout } = useAuth()
-const showLogout = ref(false)
-
-onMounted(() => {
-	// Only show logout button on mainnet
-	showLogout.value = isMainnet()
-})
 </script>
 
 <template>
@@ -47,12 +38,6 @@ onMounted(() => {
 			<Button @click="appStore.showCmd = true" type="secondary" size="mini">
 				<Icon name="terminal_square" size="16" color="secondary" />
 			</Button>
-
-			<ClientOnly>
-				<Button v-if="showLogout" @click="logout" type="secondary" size="mini" title="Logout">
-					<Icon name="logout" size="16" color="secondary" />
-				</Button>
-			</ClientOnly>
 		</Flex>
 	</Flex>
 </template>
